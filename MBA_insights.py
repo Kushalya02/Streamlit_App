ca8ff18ee71f8717553448c0e0939df422b24f98
Kushalya02 <156417818+Kushalya02@users.noreply.github.com>
2024-05-06 22:17:17 +0530

    Update MBA_insights.py
    A line chart depicting the sales over the period of order date, sactter plots showing shipping cost vs sales, discount impact on sales and relationship between the quantity and profit .

--- MBA_insights.py
+++ MBA_insights.py
@@ -61,8 +61,6 @@ with Order_details:
     st.plotly_chart(fig3)
    
    
-   
-   
     #Sales by country
     st.subheader ('Sales by country')
     fig4=px.pie(filtered_df,values='Sales', names='Country',hole=0.5,height=400,width=600)
@@ -74,4 +72,37 @@ with Order_details:
     fig5=px.bar(catedf,x="Country",y="Sales",color="Country")
     st.plotly_chart(fig5)
 
-   
+   #line chart to show sales over time
+    filtered_df["month_year"] = filtered_df["Order Date"].dt.to_period("M")
+    st.subheader('Sales Trend over time')
+
+    line = pd.DataFrame(filtered_df.groupby(filtered_df["month_year"].dt.strftime("%Y : %b"))["Sales"].sum()).reset_index()
+    line = line.sort_values(by="month_year")
+    fig6 = px.line(line, x = "month_year", y="Sales", labels = {"Sales": "Amount"},height=500, width = 1500,template="gridon")
+    st.plotly_chart(fig6,use_container_width=True)
+
+
+    #shipping cost vs sales 
+    data1 = px.scatter(orders, x = "Shipping Cost", y = "Sales", size = "Quantity")
+    data1['layout'].update(title="Shipping cost vs Sales",
+                        titlefont = dict(size=20),xaxis = dict(title="Shipping Cost",titlefont=dict(size=19)),
+                        yaxis = dict(title = "Sales", titlefont = dict(size=19)))
+    st.plotly_chart(data1,use_container_width=True)
+
+
+    #scatter plot to show discount impact on sales
+    data2 = px.scatter(orders, x = "Discount", y = "Sales", size = "Quantity")
+    data2['layout'].update(title="Discount impact on Sales",
+                        titlefont = dict(size=20),xaxis = dict(title="Discount",titlefont=dict(size=19)),
+                        yaxis = dict(title = "Sales", titlefont = dict(size=19)))
+    st.plotly_chart(data2,use_container_width=True)
+
+
+   #scatter plot to show relationship between quantity and profit
+    data3 = px.scatter(orders, x = "Quantity", y = "Profit", size = "Quantity")
+    data3['layout'].update(title="Relationship between Quantity and Profits using Scatter Plot.",
+                        titlefont = dict(size=20),xaxis = dict(title="Quantity",titlefont=dict(size=19)),
+                        yaxis = dict(title = "Profit", titlefont = dict(size=19)))
+    st.plotly_chart(data3,use_container_width=True)
+
+
