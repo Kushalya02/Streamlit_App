c364634e32586bfe3f707f0040fb571c5fd3b1f9
Kushalya02 <156417818+Kushalya02@users.noreply.github.com>
2024-05-06 22:07:05 +0530

    Update MBA_insights.py
    Imported necessary libraries and the orders and association rules data sets, which were put into data frames. Dashboard titles and sidebar titles were created and 2 tabs were initialized to show the visualizations of the 2 datasets separately.  The start and end date and  product category filters were added and the logic was initialized to make the filters change the dashboard.

--- MBA_insights.py
+++ MBA_insights.py
@@ -1,1 +1,40 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+import plotly.express as px
+import matplotlib.pyplot as plt
+
+#reading csv files
+orders=pd.read_csv('GlobalSuperstoreLite.csv')
+rules=pd.read_csv('association_rules.csv')
+
+st.title('Key Insights Dashboard')
+col1,col2=st.columns((2))
+#Adding dashboard filter
+st.sidebar.title("Dashboard Filters")
+
+
+
+#making tabs
+Order_details, association_rules=st.tabs(['Order Details','Market Basket Analysis Association Rules'])
+with Order_details:
+    st.header("Order Details")
+    st.write(orders)
+    #date picker
+    orders['Order Date']=pd.to_datetime(orders['Order Date'])
+    start_date=pd.to_datetime(orders['Order Date']).min()
+    end_date=pd.to_datetime(orders['Order Date']).max()
+
+    start=pd.to_datetime(st.sidebar.date_input('Pick start date',start_date))
+    end=pd.to_datetime(st.sidebar.date_input('Pick end date',end_date))
+    orders=orders[(orders['Order Date']>= start) & (orders['Order Date']<=end)].copy()
+
+    #Quantity and product category
+    
+    category = st.sidebar.multiselect('Pick your category',orders['Category'].unique())
+
+    if category:  
+        filtered_df = orders[orders["Category"].isin(category)]
+    else:
+        filtered_df = orders.copy()  
 
